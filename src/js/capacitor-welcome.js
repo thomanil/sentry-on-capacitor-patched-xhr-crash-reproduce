4625cd8822671d48787e97c76f8fa5dfce0836c7
Thomas Kjeldahl Nilsson <thomas@tknilsson.com>
2024-11-05 19:12:44 +0100

    strip it down, init sentry

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,5 +1,6 @@
 import { SplashScreen } from '@capacitor/splash-screen';
 import { Camera } from '@capacitor/camera';
+import * as Sentry from '@sentry/browser';
 
 window.customElements.define(
   'capacitor-welcome',
@@ -57,57 +58,15 @@ window.customElements.define(
     </style>
     <div>
       <capacitor-welcome-titlebar>
-        <h1>Capacitor</h1>
+        <h1>Capacitor skeleton app</h1>
       </capacitor-welcome-titlebar>
       <main>
         <p>
-          Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
-          with a single code base.
-        </p>
-        <h2>Getting Started</h2>
-        <p>
-          You'll probably need a UI framework to build a full-featured app. Might we recommend
-          <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
-        </p>
-        <p>
-          Visit <a href="https://capacitorjs.com">capacitorjs.com</a> for information
-          on using native features, building plugins, and more.
-        </p>
-        <a href="https://capacitorjs.com" target="_blank" class="button">Read more</a>
-        <h2>Tiny Demo</h2>
-        <p>
-          This demo shows how to call Capacitor plugins. Say cheese!
-        </p>
-        <p>
-          <button class="button" id="take-photo">Take Photo</button>
-        </p>
-        <p>
-          <img id="image" style="max-width: 100%">
+           Trying to init Sentry...
         </p>
       </main>
     </div>
     `;
-    }
-
-    connectedCallback() {
-      const self = this;
-
-      self.shadowRoot.querySelector('#take-photo').addEventListener('click', async function (e) {
-        try {
-          const photo = await Camera.getPhoto({
-            resultType: 'uri',
-          });
-
-          const image = self.shadowRoot.querySelector('#image');
-          if (!image) {
-            return;
-          }
-
-          image.src = photo.webPath;
-        } catch (e) {
-          console.warn('User cancelled', e);
-        }
-      });
     }
   },
 );
@@ -140,3 +99,16 @@ window.customElements.define(
     }
   },
 );
+
+
+try {
+    Sentry.init({
+        dsn: "https://61596f36cbc4ef77a50cec9f0370dad4@o4507084828049408.ingest.de.sentry.io/4507140464246864"
+        // ...
+    });
+
+} catch (e) {
+    alert("Failed to run Sentry.init(), caught exception "+e);
+}
+
+alert("Successfully ran Sentry.init()");
